9875d608e4bd1a9fe8707f2fb0c5fee152f6cf59
Vítor Alexandre <devper.vnk@gmail.com>
2022-03-25 11:37:39 -0300

    project

---
+++ PetShopApp/Migrations/20220324233700_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetShopApp.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proprietario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: true),
+                    Endereco = table.Column<string>(type: "TEXT", nullable: true),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    DtNascimento = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proprietario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Proprietario");
+        }
+    }
+}
--- PetShopApp/Startup.cs
+++ PetShopApp/Startup.cs
@@ -1,3 +1,4 @@
+//using Internal.Runtime.Augments;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,20 +15,29 @@ namespace PetShopApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<PetShopAppContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("PetShopAppContext")));
+            services.AddDbContext<PetShopAppContext>(options =>{
+                var connectionString  = Configuration.GetConnectionString("PetShopAppContext");
+
+                if(Environment.IsDevelopment()){
+                    options.UseSqlite(connectionString);
+                } else {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
