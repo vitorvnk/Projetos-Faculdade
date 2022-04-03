43b33eba6c8cccab10c154f2403b37de78d0c7b3
vitorvnk <devper.vnk@gmail.com>
2022-04-03 13:33:57 -0300

    a

---
+++ Angular-pwa/src/app/services/model/Seguro.ts
@@ -0,0 +1,11 @@
+import { MarcaCarro } from './MarcaCarro';
+
+export class Seguro {
+  marcaCarro: MarcaCarro | undefined;
+  modeloCarro: string | undefined;
+  placaCarro: string | undefined;
+  nomeProprietario: string | undefined;
+  sobrenomeProprietario: string | undefined;
+  dataNascimentoProprietario: string | undefined;
+
+}
