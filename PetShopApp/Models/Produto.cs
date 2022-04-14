ec0066c35775ad5d23a819d7c3e127e0c48fae0a
Vítor Alexandre <devper.vnk@gmail.com>
2022-04-14 20:21:01 -0300

    aula 14-04

--- PetShopApp/Models/Produto.cs
+++ PetShopApp/Models/Produto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System;
 
@@ -6,10 +7,23 @@ namespace PetShopApp.Models
     public class Produto
     {
         public int Id {get; set;}
+
         public string Nome {get; set;}
+
+        [Display(Name = "Descrição")]
         public string DescricaoProduto {get; set;}
+        
+        [Display(Name = "Quantidade")]
         public int QtdProduto {get; set;}
+        
+        [Display(Name = "Preço de Compra")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public float PrecoCompra {get; set;}
+
+        [Display(Name = "Preço de Venda")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public float PrecoVenda {get; set;}
     }
 }
